3bdbbaa76efa476060b6449a5378a2d1ea47e0af
MarioHarisis <marioharisis27@gmail.com>
2025-03-27 18:51:32 +0100

    Hello World!

---
+++ Learning-Solidity/contracts/Hello.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+ 
+ // Define la versión de Solidity que puede compilar el contrato.
+ pragma solidity >=0.8.2 <0.9.0;
+
+ contract HelloWorld {
+
+    string public welcome = "Hello World!";
+ }
